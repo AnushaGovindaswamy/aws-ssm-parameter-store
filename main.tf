5b78cdbf47da6f223a1a9f1a57bcd4f932d1e09f
anusha <anushag2k16@gmail.com>
2023-05-13 15:53:41 +0530

    committing to learn-ansible

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_ssm_parameter" "parameters" {
+  count  = length(var.parameters)
+  name   = var.parameters[count.index].name
+  value  = var.parameters[count.index].value
+  type   = "String"
+  key_id = "f47a8305-1229-455f-a1bd-0f925a772631"
+}
